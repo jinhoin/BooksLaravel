4452c3b4c56714bb4f428c3888923392a0f751df
jinhoin <41941147+jinhoin@users.noreply.github.com>
2019-01-21 18:39:32 +0900

    chapter3 완성

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('/', function () {
+//     //문자로도 반환가능
+//     return "<h1>hello Foo</h1>";
+// });
+// 파마리멑를 정규표현을 써서 제한시켰다 없을경우 bar가 대체한다
+Route::pattern('foo', '[0-9a-zA-Z]{3}');
+// // 파마리터를 바로 받아서 나타낼수도있다 세글자가 아닐경우 제외한다
+Route::get('/{foo?}', function ( $foo = 'bar') {
+    return "{$foo}";
+});
+
+Route::get('/', [
+    'as' => 'home',
+    function (){
+        return '제이름은 Home입니다';
+    }
+]);
+
+Route::get('/home',  function (){
+    return redirect(route('home'));
 });
